9a6ce606731e3226d309ab3454b0f443e47677f1
SubastralAlok <alokkumaryadav241004@gmail.com>
2025-12-27 17:46:43 +0530

    Phase 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ContactForm from './Components/ContactForm/ContactForm'
 
 const App = () => {
   return (
-    <div className='w-full min-h-screen bg-(--bg-main) p-4'>
+    <div className='w-full min-h-screen bg-(--btn-main)/30 p-4'>
       <Navbar />
       <ContactForm />
     </div>
