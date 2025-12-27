91f51576b7ad8511b2d08039f3649526cb5a78ab
SubastralAlok <alokkumaryadav241004@gmail.com>
2025-12-27 16:48:06 +0530

    Made Navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center bg-(--bg-card) backdrop-blur-3xl p-4 shadow-md rounded-lg'>
+      <div>
+        <h1 className='text-3xl text-(--text-main) font-semibold'>BuddyBook</h1>
+      </div>
+        <div className='hidden sm:flex gap-4'>
+            <a href="#" className='bg-(--btn-main) text-(--text-sec) px-4 py-2 font-light rounded hover:bg-(--btn-sec)/80 hover:text-(--bg-main) transition-all duration-300'>Home</a>
+            <a href="#" className='bg-(--btn-main) text-(--text-sec) px-4 py-2 font-light rounded hover:bg-(--btn-sec)/80 hover:text-(--bg-main) transition-all duration-300'>All Contacts</a>
+            <a href="#" className='bg-(--btn-main) text-(--text-sec) px-4 py-2 font-light rounded hover:bg-(--btn-sec)/80 hover:text-(--bg-main) transition-all duration-300'>About us</a>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
